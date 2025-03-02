16f9b9659f131e4795fd8c3f973467db5e09cc79
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2025-03-02 10:50:37 +0530

    fix: Image upload by v7 uploadthing migration

--- apps/client/app/api/uploadthing/route.ts
+++ apps/client/app/api/uploadthing/route.ts
@@ -1,11 +1,10 @@
-import { createRouteHandler } from 'uploadthing/next';
+import { createRouteHandler } from "uploadthing/next";
 
-import { ourFileRouter } from './core';
+import { ourFileRouter } from "./core";
 
 // Export routes for Next App Router
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
 
-  // Apply an (optional) custom config:
-  // config: { ... },
+  config: { token: process.env.UPLOADTHING_TOKEN },
 });
